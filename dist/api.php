8db4c433ea14f958558ac66a43d70d989ba2adb1
Axel Chalon <axel.chalon@hetic.net>
2015-11-21 21:22:55 +0100

    backend fini

--- dist/api.php
+++ dist/api.php
@@ -1,5 +1,18 @@
 <?php
 
+/*
+# ROUTES
+GET words
+GET words/joie/items
+PUT items/23/star
+POST sign_in
+*/
+
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+session_start();
+
 require_once __DIR__.'/../vendor/autoload.php';
 require_once __DIR__.'/../conf/config.php';
 
@@ -76,6 +89,100 @@ $app->get('api/v1/words/{word}/items', function ($word) use ($app) { // @todo NO
 	
 	*/
 	return $app->json(array('items' => $flattened_items));
+});
+
+$app->put('api/v1/items/{item_id}/star', function ($item_id) use ($app) { // @todo NODB ?
+	
+	if (!isset($_SESSION['username']))
+		return new Response('Not logged in.',401);
+	
+	$q = $app['db']->prepare('SELECT * FROM user_stars WHERE user_id = :user_id AND item_id = :item_id');
+	$q->execute(array(
+		':user_id' => $_SESSION['user_id'],
+		':item_id' => $item_id,
+	));
+	
+	if ($q->fetch() === false) // item not starred yet
+	{
+		$q = $app['db']->prepare('INSERT INTO user_stars VALUES (:user_id, :item_id)');
+		$q->execute(array(
+			':user_id' => $_SESSION['user_id'],
+			':item_id' => $item_id,
+		));
+	}
+	
+	return new Response('Star added.',200);
+});
+
+$app->delete('api/v1/items/{item_id}/star', function ($item_id) use ($app) { // @todo NODB ?
+	
+	if (!isset($_SESSION['username']))
+		return new Response('Not logged in.',401);
+	
+	$q = $app['db']->prepare('DELETE FROM user_stars WHERE user_id = :user_id AND item_id = :item_id');
+	$q->execute(array(
+		':user_id' => $_SESSION['user_id'],
+		':item_id' => $item_id,
+	));
+	
+	return new Response('Star deleted.',200);
+});
+
+$app->post('api/v1/sign_in', function (Request $request) use ($app) { // @todo NODB ? 
+	$q = $app['db']->prepare('SELECT id, username FROM users WHERE (username = :username OR mail = :mail) AND password = :password');
+	$q->execute(array(
+		':username' => $request->get('login'),
+		':mail' => $request->get('login'),
+		':password' => md5(SALT . $request->get('password')),
+	));
+	
+	
+	$f = $q->fetch();
+	if ($f === false)
+		return new Response('Wrong login or password.',401);
+
+	$_SESSION['username'] = $f['username'];
+	$_SESSION['user_id'] = $f['id'];
+		
+	return new Response('Signed in.',200);
+	
+});
+
+$app->post('api/v1/sign_up', function (Request $request) use ($app) { // @todo NODB ?
+	
+	if (!filter_var($request->get('mail'), FILTER_VALIDATE_EMAIL))
+		return new Response('Invalid mail address.',400);
+	else if (strlen($request->get('username')) < 1)
+		return new Response('Username too short.',400);
+	else if (strlen($request->get('password')) < 4)
+		return new Response('Password too short.',400);
+	
+	$q = $app['db']->prepare('SELECT id, username FROM users WHERE username = :username OR mail = :mail');
+	$q->execute(array(
+		':username' => $request->get('username'),
+		':mail' => $request->get('mail')
+	));
+	
+	if ($q->fetch() !== false)
+		return new Response('User already exists.',400);
+
+	$q = $app['db']->prepare('INSERT INTO users(username,mail,password) VALUES(:username,:mail,:password)');
+	$q->execute(array(
+		':username' => $request->get('username'),
+		':mail' => $request->get('mail'),
+		':password' => md5(SALT . $request->get('password'))
+	));
+	
+	$_SESSION['username'] = $request->get('username');
+	$_SESSION['user_id'] = $app['db']->lastInsertId();
+		
+	return new Response('Signed up and signed in.',200);
+	
+});
+
+$app->get('api/v1/debug', function () use ($app) { // @todo NODB ?
+	var_dump($_SESSION);
+	return '';
 });
 
 $app->run();
