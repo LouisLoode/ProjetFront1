0e0afa45c8ac414e6498077d88b471cb5ddee369
LouisLoode <contact@ldbrn.com>
2015-11-22 14:44:42 +0100

    Ajout de la fonction clean dans le gulp (elle ne supprime pas dist/.htaccess, dist/api.php et dist/index.html

--- gulpfile.js
+++ gulpfile.js
@@ -11,7 +11,6 @@ var gulp = require('gulp'),
     notify = require('gulp-notify'),
     livereload = require('gulp-livereload'),
     bower = require('gulp-bower'),
-    // usemin = require('gulp-usemin'), // à améliorer
     del = require('del'),
 	promise = require('es6-promise');
 
@@ -50,19 +49,21 @@ gulp.task('images', function() {
     .pipe(notify({ message: 'Images task complete' }));
 });
 
-/*gulp.task('usemin', function() {
-  return gulp.src(['src/*.html', 'src/*.php'])
-    .pipe(usemin({
-      js: [ uglify ]
-    }))
-    .pipe(gulp.dest('dist/'))
-    .pipe(notify({ message: 'usemin task complete' }));
-});*/
-
 // On supprime la distrib qu'on a créé
-gulp.task('clean', function() {
-    return del(['dist']);
+gulp.task('clean', function () {
+  return del([
+    'dist/fonts',
+    'dist/img',
+    'dist/js',
+    'dist/styles',
+    'dist/index.html',
+    // we don't want to clean this file though so we negate the pattern
+    '!dist/.htaccess',
+    '!dist/index.html',
+    '!dist/api.php'
+  ]);
 });
+
 
 // On observe le changement de fichier pour recharger la page
 gulp.task('watch', function() {
@@ -75,12 +76,6 @@ gulp.task('watch', function() {
 
   // Watch image files
   gulp.watch('src/img/**/*', ['images']);
-    
-  // Watch the html files
-  //gulp.watch('src/{,partial/}/*.html', ['extend']);
-  // gulp.watch('src/*.html', ['usemin']);
-  // gulp.watch('src/*.php', ['usemin']);
-  
     
   // Create LiveReload server
   livereload.listen();
