7f0e1c6c194c6b50c28d6d73ad085ea02b75b813
Axel Chalon <axel.chalon@hetic.net>
2015-11-25 22:52:30 +0100

    UI inscription/connexion OK

--- src/js/app/App.js
+++ src/js/app/App.js
@@ -34,8 +34,8 @@ var App = function() {
 	}.bind(this));
 	
 	this.Vue_TopBar = new Vue({
-		el: 'aside',
-		data: {username: null}
+		el: '.navbar',
+		data: {username: null, user_id: -1}
 	});
 
 	this.getAndRefreshAuthenticationInfo();
@@ -54,6 +54,7 @@ App.prototype.getAndRefreshAuthenticationInfo = function() {
 					this.mail = data.mail;
 
 					this.Vue_TopBar.username = data.username;
+					this.Vue_TopBar.user_id = data.user_id;
 
 
 					$('body').addClass('signed-in');
@@ -63,9 +64,14 @@ App.prototype.getAndRefreshAuthenticationInfo = function() {
 }
 
 App.prototype.bindEvents = function () {
-	$('.user-greeting .me').click(function() {
+	/*$('.user-greeting .me').click(function() {
 		this.router.navigate('/profil/' + this.user_id);
 		return false;
+	}.bind(this));*/
+	
+	$('.sign-in-button').click(function(){
+		$('.subnav').removeClass('active');
+		$('.sign-in-subnav').addClass('active');
 	}.bind(this));
 	
 	$('.sign-in').submit(function(e){
@@ -76,6 +82,7 @@ App.prototype.bindEvents = function () {
             data: $(e.target).serialize(),
             success: function(response) {
 				this.getAndRefreshAuthenticationInfo();
+				$('.subnav').removeClass('active');
             }.bind(this),
 			error: function(error) {
 				console.error(error);	
@@ -83,6 +90,11 @@ App.prototype.bindEvents = function () {
         });
 		
 		return false;
+	}.bind(this));
+	
+	$('.sign-up-button').click(function(){
+		$('.subnav').removeClass('active');
+		$('.sign-up-subnav').addClass('active');
 	}.bind(this));
 	
 	$('.sign-up').submit(function(e){
@@ -93,6 +105,7 @@ App.prototype.bindEvents = function () {
             url: $(e.target).attr("action"),
             data: $(e.target).serialize(),
             success: function(response){
+				$('.subnav').removeClass('active');
 				this.getAndRefreshAuthenticationInfo();
             }.bind(this),
 			error: function(error) {
@@ -108,11 +121,14 @@ App.prototype.bindEvents = function () {
 					type: "POST",
 					url: '/api/v1/sign_out',
 					success: function () {
-						$('body').removeClass('signed-in');
+						this.Vue_TopBar.username = '';
+						this.Vue_TopBar.user_id = -1;
+						$('.subnav').removeClass('active');
 					}.bind(this)
 			});
 		return false;
 	}.bind(this));
+	
 }
 
 App.prototype.goTo = function (newPage, method) {
