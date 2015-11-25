9b39c0ef0ae33df78a4801e028607b7a6ce3e29d
Axel Chalon <axel.chalon@hetic.net>
2015-11-25 23:38:22 +0100

    début intégration item focus

--- src/js/app/Page_ExploreItems.js
+++ src/js/app/Page_ExploreItems.js
@@ -1,7 +1,7 @@
 var Page_ExploreItems = function() {
 	this.Vue = new Vue({
 		el: '.explore-items', // @todo cohérence où est nommée la page
-		data: {items: {}, word: ''} // @todo add items[0].starred = true/false
+		data: {items: {}, word: '', focus: {}} // @todo add items[0].starred = true/false
 	});
 	
 	Page.apply(this, arguments);
@@ -52,11 +52,22 @@ Page_ExploreItems.prototype.bindEvents = function () {
 		this.exploreSimilarItems(parseInt($(e.target).data('id')));
 	}.bind(this));
 	
+	var that = this;
 	$('.explore-items').on('click','.item', function(e) {
+		var focus_id = $(this).data('id');
+		that.Vue.focus = that.Vue.items.filter(function(a){ return a.id == focus_id; })[0];
 		$('.item-focus').removeClass('hidden');
 		// $('.item-focus img').attr('src',$(e.target).find('img').attr('src'));
 		console.log($(this).data('id'));
 		$('.item-focus .star, .item-focus .similar').attr('data-id',$(this).data('id'));
+		
+		$.ajax({
+			type: "GET",
+			url: '/api/v1/items/' + focus_id +'/words',
+			success: function(data) {
+				that.Vue.focus.words = data.words;
+			}
+		});
 	});
 	
 	setTimeout(function() {
