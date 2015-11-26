9040bf634edf82ef4379abba75e0ed1d2c59a5a7
Axel Chalon <axel.chalon@hetic.net>
2015-11-27 00:59:42 +0100

    parallaxe infini maison OK

--- conf/config.php
+++ conf/config.php
@@ -1,7 +1,7 @@
 <?php
 
-define('DB_HOST','');
-define('DB_NAME','');
-define('DB_USERNAME','');
-define('DB_PASSWORD','');
-define('SALT','');
+define('DB_HOST','141.138.157.211');
+define('DB_NAME','words');
+define('DB_USERNAME','words');
+define('DB_PASSWORD','orane');
+define('SALT','jwgK!bK-cL-$QxUFzksC_aoH!C!IMECW');
